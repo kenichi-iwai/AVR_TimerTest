c5970a1516298020fb3830a4323971ce4537a7a4
Kenichi Iwai <kenichi.iwai@eynet.jp>
2023-08-25 18:16:47 +0900

    first commit

---
+++ AVR_TimerTest/main.c
@@ -0,0 +1,18 @@
+/*
+ * AVR_TimerTest.c
+ *
+ * Created: 2023/08/25 金 18:15:17
+ * Author : Kenichi Iwai
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
